07d89a648c1dbe180cf426f4186654b983438371
Michał Darda <michaldarda@gmail.com>
2016-07-27 22:43:22 +0200

    Fix custom class names, introduce rubocop

--- lib/factory_boy.rb
+++ lib/factory_boy.rb
@@ -1,76 +1,54 @@
-require "factory_boy/version"
+require 'factory_boy/version'
 
 module FactoryBoy
   class FactoryNotFound < StandardError; end
   class Factory
-   attr_reader :attributes 
-    
+    attr_reader :attributes
+
     def initialize(name)
       @name = name
-      
+
       @attributes = {}
     end
-    
-    def method_missing(meth, *args, &blk)
+
+    def method_missing(meth, *args)
       @attributes[meth] = args.first
     end
   end
-    
+
   def self.factories
     @factories ||= {}
-  end 
-  
+  end
+
+  # name can be :user or User, there could also be passed
+  # class option
   def self.define_factory(name, options = {}, &block)
-    klass = if options[:class]
-      options[:class] 
-    elsif name.is_a?(Symbol)
-      const_get(name.to_s.capitalize) 
-    end
-    
+    klass = options.fetch(:class) { const_get(name.to_s.capitalize) }
+
     factory_instance = Factory.new(klass)
-    
-    store_factory(name, factory_instance, klass)
-    
+
+    factories[name.to_s.downcase.to_sym] = {
+      class: klass,
+      instance: factory_instance
+    }
+
     factory_instance.instance_eval(&block) if block_given?
+
     factory_instance
   end
-  
-  def self.build(klass, attributes = {})
-    factory = if klass.is_a?(Symbol)
-      const_get(klass.to_s.capitalize) rescue factories[klass]
-    else
-      find_factory(klass) 
-    end
-    
-    factory or raise FactoryNotFound
-    
-    klass = factory[:klass]
-    
-    factory = factory[:instance] if factory
-  
-    instance_attributes = factory.attributes.merge(attributes)
-   
-    instance = klass.new
-      
+
+  def self.build(name, attributes = {})
+    name = name.to_s.downcase.to_sym
+
+    (factory = factories[name]) || raise(FactoryNotFound)
+
+    instance_attributes = factory.fetch(:instance).attributes.merge(attributes)
+    instance = factory.fetch(:class).new
+
     instance_attributes.each do |attribute, value|
       instance.send("#{attribute}=", value)
     end
-      
+
     instance
   end
-  
-  def self.find_factory(factory_name, klass = nil)
-    factory_instance = factories[factory_name]
-    factory_instance && factory_instance[:instance]
-  end
-  
-  def self.store_factory(factory_name, factory_instance, klass = nil)
-    factory_data = {
-      instance: factory_instance,
-      klass: klass
-    }
-    
-    factories[factory_name] = factory_data
-    factories[klass] = factory_data if klass
-  end 
 end
--- spec/factory_boy_spec.rb
+++ spec/factory_boy_spec.rb
@@ -2,76 +2,77 @@ require 'spec_helper'
 
 describe FactoryBoy do
   before do
-    FactoryBoy.instance_variable_set("@factories", {})
+    FactoryBoy.instance_variable_set('@factories', {})
   end
-  
+
   it 'allows to define factory' do
     User = Struct.new(:name)
-    
+
     user_factory = FactoryBoy.define_factory(User)
-    
+
     expect(user_factory).to be_kind_of(FactoryBoy::Factory)
   end
-  
+
   it 'allows to build instance of given class' do
     User = Struct.new(:name)
-    
+
     FactoryBoy.define_factory(User)
     instance = FactoryBoy.build(User)
-    
+
     expect(instance).to be_kind_of(User)
   end
-  
+
   it 'allows to pass custom parameters' do
     User = Struct.new(:name)
-    
+
     FactoryBoy.define_factory(User)
-    instance = FactoryBoy.build(User, name: "Michał")
-    
-    expect(instance.name).to eq "Michał"
+    instance = FactoryBoy.build(User, name: 'Michał')
+
+    expect(instance.name).to eq 'Michał'
   end
-  
+
   it 'raises exception if factory is not found' do
-    expect{FactoryBoy.build(User, name: "Michał")}.to raise_error(FactoryBoy::FactoryNotFound)
-  end 
-  
+    expect { FactoryBoy.build(User, name: 'Michał') }
+      .to raise_error(FactoryBoy::FactoryNotFound)
+  end
+
   it 'allows to define value for attributes' do
     User = Struct.new(:name)
-    
+
     FactoryBoy.define_factory(User) do
-      name "Michał"
+      name 'Michał'
     end
-    
+
     instance = FactoryBoy.build(User)
-    
-    expect(instance.name).to eq "Michał"
-  end 
-  
+
+    expect(instance.name).to eq 'Michał'
+  end
+
   it 'allows to define value for attributes' do
     User = Struct.new(:name)
-    
+
     FactoryBoy.define_factory(User) do
-      name "Michał"
+      name 'Michał'
     end
-    
-    instance = FactoryBoy.build(User, name: "Przemysław")
-    
-    expect(instance.name).to eq "Przemysław"
-  end 
-  
+
+    instance = FactoryBoy.build(User, name: 'Przemysław')
+
+    expect(instance.name).to eq 'Przemysław'
+  end
+
   it 'allows to pass symbol instead of class name to define_factory' do
     FactoryBoy.define_factory(:user)
-    
+
     instance = FactoryBoy.build(:user)
-    
+
     expect(instance).to be_kind_of(User)
-  end 
-  
+  end
+
   it 'allows to pass alias class name to define_factory' do
     FactoryBoy.define_factory(:admin, class: User)
-    
+
     instance = FactoryBoy.build(:admin)
-    
+
     expect(instance).to be_kind_of(User)
-  end 
+  end
 end
